8783670afc86754634ecae69c5eeb0a7158435b8
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-06-03 14:57:29 +0100

    Create Feed Item Model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vera Dias on 03/06/2024.
+//
+
+import Foundation
+
+struct FeedItem {
+	let id: UUID
+	let description: String?
+	let location: String?
+	let imageURL: URL
+}
