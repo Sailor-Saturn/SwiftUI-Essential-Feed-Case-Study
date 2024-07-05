66d20801fd1b2bb2411a3bbd04eda73bcfdb2e2d
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-07-05 14:28:33 +0100

    Give a URL instead of hard coding it in the remote feed loader

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,14 +8,16 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let url: URL
     let client: HTTPClient
 
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
 
     func load() {
-        client.get(from: URL(string: "https://www.any.com")!)
+        client.get(from: url)
     }
 }
 protocol HTTPClient {
@@ -33,18 +35,20 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "https://www.any.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
 
         XCTAssertEqual(client.requestedURL, nil)
     }
 
     func test_load_requestsDataFromURL() {
+        let url = URL(string: "https://www.any.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
 
         sut.load()
 
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
