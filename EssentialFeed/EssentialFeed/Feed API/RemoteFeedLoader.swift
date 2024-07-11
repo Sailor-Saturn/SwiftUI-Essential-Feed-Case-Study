2c0ef4866eecd02a3f7bbe950b66640660e3b622
Sailor-Saturn <vera.ricardina.dias@gmail.com>
2024-07-11 10:06:11 +0100

    Delivers invalid data error on non 200 http responses

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -6,6 +6,7 @@ public final class RemoteFeedLoader {
 
     public enum Error: Swift.Error {
         case connectivity
+        case invalidData
     }
 
     public init(url: URL, client: HTTPClient) {
@@ -14,13 +15,18 @@ public final class RemoteFeedLoader {
     }
 
     public func load(completion: @escaping(Error) -> Void) {
-        client.get(from: url) { error in
+        client.get(from: url) { error, response in
+            if response != nil {
+                completion(.invalidData)
+            } else {
+                completion(.connectivity)
+            }
             completion(.connectivity)
         }
     }
 }
 
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error) -> Void)
+    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
 }
 
